8ed5b573e5925f2a210d3153ca51dbee3b087a90
Hari Prasad <hariprasad12994@gmail.com>
2022-04-27 17:20:39 +0530

    DP practise - updated LCS

--- lcs.cpp
+++ lcs.cpp
@@ -1,25 +1,60 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <map>
 
-unsigned int longest_common_subsequence(std::string seq_1, std::string seq_2, std::size_t seq_1_idx, std::size_t seq_2_idx){
-    // std::cout << "Comparing " << seq_1 << " and " << seq_2 << '\n';
+struct Index {
+public:
+  int idx_1;
+  int idx_2;
+  Index(int idx_1_, int idx_2_): idx_1(idx_1_), idx_2(idx_2_) {}
+  friend bool operator<(const Index& lhs, const Index& rhs) {
+    return lhs.idx_1 < rhs.idx_1 || (lhs.idx_1 == rhs.idx_1 && lhs.idx_2 < rhs.idx_2);
+  }
+};
 
-    if(seq_1_idx == 0 || seq_2_idx == 0)
-        return 0;
 
-    if(seq_1.at(seq_1_idx - 1) == seq_2.at(seq_2_idx - 1))
-        return 1 + longest_common_subsequence(seq_1.substr(0, seq_1_idx - 1), seq_2.substr(0, seq_2_idx - 1), seq_1_idx - 1, seq_2_idx - 1);
+unsigned int lcs_memo(const std::string& seq_1, const std::string& seq_2, std::size_t seq_1_idx, std::size_t seq_2_idx, std::map<Index, int>& memo) {
+  Index idx(seq_1_idx, seq_2_idx);
 
-    return std::max(longest_common_subsequence(seq_1.substr(0, seq_1_idx - 1), seq_2.substr(0, seq_2_idx), seq_1_idx - 1, seq_2_idx),
-            longest_common_subsequence(seq_1.substr(0, seq_1_idx), seq_2.substr(0, seq_2_idx - 1), seq_1_idx, seq_2_idx - 1));
+  if(memo.find(idx) != memo.end()) return memo[idx];
+
+  if(seq_1_idx == 0 || seq_2_idx == 0) return 0;
+
+  if(seq_1.at(seq_1_idx - 1) == seq_2.at(seq_2_idx - 1)) {
+    memo[idx] = 1 + lcs_memo(seq_1, seq_2, seq_1_idx - 1, seq_2_idx - 1, memo);
+    return memo[idx];
+  }
+
+  memo[idx] = std::max(lcs_memo(seq_1, seq_2, seq_1_idx - 1, seq_2_idx, memo),
+                       lcs_memo(seq_1, seq_2, seq_1_idx, seq_2_idx - 1, memo));
+  return memo[idx];
+}
+
+unsigned int lcs_memo(const std::string& seq_1, const std::string& seq_2, std::size_t seq_1_idx, std::size_t seq_2_idx){
+  std::map<Index, int> memo;
+  return lcs_memo(seq_1, seq_2, seq_1_idx, seq_2_idx, memo);
+}
+
+unsigned int lcs(const std::string& seq_1, const std::string& seq_2, std::size_t seq_1_idx, std::size_t seq_2_idx){
+  if(seq_1_idx == 0 || seq_2_idx == 0) return 0;
+
+  if(seq_1.at(seq_1_idx - 1) == seq_2.at(seq_2_idx - 1))
+    return 1 + lcs(seq_1, seq_2, seq_1_idx - 1, seq_2_idx - 1);
+
+  return std::max(lcs(seq_1, seq_2, seq_1_idx - 1, seq_2_idx),
+                  lcs(seq_1, seq_2, seq_1_idx, seq_2_idx - 1));
 }
 
 int main(void){
-    std::cout << longest_common_subsequence(std::string("ABDACDA"), std::string("ABADCE"), 7, 6) << '\n';
-    std::cout << longest_common_subsequence(std::string("aggtab"), std::string("gxtxayb"), 6, 7) << '\n';
-    std::cout << longest_common_subsequence(std::string("ABAA"), std::string("ABADCE"), 4, 6) << '\n';
+  std::cout << lcs(std::string("ABDACDA"), std::string("ABADCE"), 7, 6) << '\n';
+  std::cout << lcs(std::string("aggtab"), std::string("gxtxayb"), 6, 7) << '\n';
+  std::cout << lcs(std::string("ABAA"), std::string("ABADCE"), 4, 6) << '\n';
 
-    return 0;
+  std::cout << lcs_memo(std::string("ABDACDA"), std::string("ABADCE"), 7, 6) << '\n';
+  std::cout << lcs_memo(std::string("aggtab"), std::string("gxtxayb"), 6, 7) << '\n';
+  std::cout << lcs_memo(std::string("ABAA"), std::string("ABADCE"), 4, 6) << '\n';
+
+  return 0;
 }
 
