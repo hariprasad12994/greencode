835872e84b4f3deeec90c6c045b4070ff5644366
Hari Prasad <hariprasad12994@gmail.com>
2022-05-25 17:09:32 +0530

    Blind 75, minimum element in sorted rotated array

--- dsa/arrays/min_in_sorted_array.cpp
+++ dsa/arrays/min_in_sorted_array.cpp
@@ -4,14 +4,17 @@
 
 int min_element_by_bsearch(const std::vector<int>& nums, std::size_t left_idx, std::size_t right_idx) {
   auto mid_idx = (left_idx + right_idx) / 2;
-  for(std::size_t iter = left_idx; iter <= right_idx; iter++) std::cout << nums[iter] << " ";
-  std::cout << "mid = " << nums[mid_idx] << '\n';
 
-  if(mid_idx == 0 || mid_idx == nums.size() - 1) return nums[mid_idx];
+  if(mid_idx == 0 || mid_idx == nums.size() - 1) 
+    return nums[mid_idx];
   if(nums[mid_idx] < nums[mid_idx - 1] && nums[mid_idx + 1] > nums[mid_idx])
     return nums[mid_idx];
-  if(nums[left_idx] > nums[right_idx])
-    return min_element_by_bsearch(nums, mid_idx + 1, right_idx);
+  if(nums[left_idx] > nums[right_idx]) {
+    if(nums[mid_idx] > nums[right_idx])
+      return min_element_by_bsearch(nums, mid_idx + 1, right_idx);
+    if(nums[mid_idx] < nums[left_idx])
+      return min_element_by_bsearch(nums, left_idx, mid_idx - 1);
+  }
   return min_element_by_bsearch(nums, left_idx, mid_idx - 1);
 }
 
@@ -24,15 +27,12 @@ int min_element_in_sorted_rotated_array(const std::vector<int>& nums) {
 
 int main(void) {
   std::cout << min_element_in_sorted_rotated_array(std::vector<int>{9}) << '\n';
-  std::cout << '\n';
   std::cout << min_element_in_sorted_rotated_array(std::vector<int>{7, 5, 4, 1, 2}) << '\n';
-  std::cout << '\n';
   std::cout << min_element_in_sorted_rotated_array(std::vector<int>{1, 2, 3, 4, 5}) << '\n';
-  std::cout << '\n';
   std::cout << min_element_in_sorted_rotated_array(std::vector<int>{5, 4, 3, 1}) << '\n';
-  std::cout << '\n';
   std::cout << min_element_in_sorted_rotated_array(std::vector<int>{13, 22, 46, 107, 112, 3, 7}) << '\n';
-  std::cout << '\n';
   std::cout << min_element_in_sorted_rotated_array(std::vector<int>{53, 134, 181, 343, 540, 604, 777, 781, 3, 7, 13, 17, 19}) << '\n';
+  std::cout << min_element_in_sorted_rotated_array(std::vector<int>{1, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10, 10, 11}) << '\n';
+  std::cout << min_element_in_sorted_rotated_array(std::vector<int>{11, 11, 11, 7, 7, 7, 7, 5, 4, 2, 1, 0}) << '\n';
   return 0;
 }
