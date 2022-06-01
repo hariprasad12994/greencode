c41126895dea2e05c755e686f3b138931c131931
Hari Prasad <hariprasad12994@gmail.com>
2022-06-01 12:13:31 +0530

    Classic binary search

--- dsa/arrays/binary_search.cpp
+++ dsa/arrays/binary_search.cpp
@@ -2,10 +2,24 @@
 #include <vector>
 
 
-std::size_t binary_search(const std::vector<int>& nums, int key) {
+int binary_search(const std::vector<int>& nums, int key, std::size_t left, std::size_t right) {
+  if(left >= right) return -1;
+
+  auto mid = (left + right) / 2;
+  if(nums[mid] == key) return mid;
+  if(nums[mid] > key) 
+    return binary_search(nums, key, mid + 1, right);
+  return binary_search(nums, key, left, mid - 1);
+}
+
+
+int binary_search(const std::vector<int>& nums, int key) {
+  return binary_search(nums, key, 0, nums.size() - 1);
 }
 
 
 int main(void) {
+  std::cout << binary_search(std::vector<int>{8, 9, 15, 65, 109, 111, 203}, 76) << '\n';
+  std::cout << binary_search(std::vector<int>{8, 9, 15, 65, 109, 111, 203}, 65) << '\n';
   return 0;
 }
