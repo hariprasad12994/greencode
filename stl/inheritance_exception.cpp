3b2377cef06705e248cc6ca18702fe62af325d71
Hari Prasad <hariprasad12994@gmail.com>
2022-08-07 13:50:26 +0530

    Illustration on object destruction while throwing exception in ctor

---
+++ stl/inheritance_exception.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stdexcept>
+
+
+class FirstClass {
+  public:
+    FirstClass(const std::string& param) {
+      if(param.size() < 5)
+        throw std::invalid_argument("String length not sufficient for FirstClass object construction");
+      member = param;
+    }
+    ~FirstClass() { std::cout << "[~FirstClass] " << member << '\n'; }
+
+  private:
+    std::string member;
+};
+
+class SecondClass: public FirstClass {
+  public:
+    SecondClass(const std::string& parent_param, const std::string& child_param): FirstClass(parent_param), member(child_param) {}
+    ~SecondClass() { std::cout << "[~SecondClass] " << member << '\n'; }
+  
+  private:
+    std::string member;
+};
+
+
+class ThirdClass: public FirstClass {
+  public:
+    ThirdClass(const std::string& parent_param, const std::string& child_param): FirstClass(parent_param) {
+      if(child_param.size() < 3) 
+        throw std::invalid_argument("String length not sufficient for ThirdClass object construction");
+      member = child_param;
+    }
+    ~ThirdClass() { std::cout << "[~ThirdClass] " << member << '\n'; }
+
+  private:
+    std::string member;
+};
+
+
+int main(void) {
+  FirstClass obj_1(std::string("hello"));
+  try {
+    // Note that the object is not yet constructed, so destructor will not be
+    // called
+    FirstClass obj_2(std::string("eat"));
+  } 
+  catch (const std::exception& e) {
+    std::cout << e.what() << '\n';
+  }
+
+  SecondClass obj_3(std::string("hello"), std::string("world"));
+  try {
+    // Note that the object is not yet constructed, so destructor will not be
+    // called for both the parent and the child class. In this case the
+    // exception is in the parent class
+    SecondClass obj_4(std::string("eat"), std::string("apple"));
+  }
+  catch (const std::exception& e) {
+    std::cout << e.what() << '\n';
+  }
+
+  ThirdClass obj_5(std::string("jibber"), std::string("jabber"));
+  try {
+    // Note that the ThirdClass object is not yet constructed so the destructor
+    // of the ThirdClass object will not be called. But the inherited parent
+    // class FirstClass was constructed succesfully, so the destructor for
+    // "Nemesis" will be called while going out of scope
+    ThirdClass obj_6(std::string("Nemesis"), std::string("oh"));
+  }
+  catch (const std::exception& e) {
+    std::cout << e.what() << '\n';
+  }
+
+  return 0;
+}
