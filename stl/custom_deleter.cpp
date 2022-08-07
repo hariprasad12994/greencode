45c6dd45634a8a04d403519edb9ce24609a056b6
Hari Prasad <hariprasad12994@gmail.com>
2022-08-07 12:14:00 +0530

    Initial stl illustration - Custom unique_ptr deleter

---
+++ stl/custom_deleter.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+#include <exception>
+#include <string>
+
+
+struct FileDeleter {
+  void operator()(FILE* file) {
+    fclose(file);
+    std::cout << "Closing file" << '\n';
+  }
+};
+
+
+class FileHandler {
+  public:
+    FileHandler(const char* file_path, const char* mode): file_ptr(fopen(file_path, mode)) {}
+  
+  private:
+    std::unique_ptr<FILE, FileDeleter> file_ptr;
+};
+
+
+int main(void) {
+  FileHandler f("hello.txt", "w");
+  try {
+    std::string("abc").substr(10);
+  }
+  catch (const std::exception& e) {
+    std::cout << e.what() << '\n';
+    throw;
+  }
+  return 0;
+}
